b9b76cbd516ae49f26d6a6bb2b8cc73d432e00fa
davidivad96 <davidivad96@gmail.com>
2021-11-02 22:29:58 +0100

    Define Lambda function to deploy with CDK

--- lib/cdk-hello-world-stack.ts
+++ lib/cdk-hello-world-stack.ts
@@ -1,7 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class CdkHelloWorldStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // defines an AWS Lambda resource
+    const hello = new NodejsFunction(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handler',
+      entry: path.join(__dirname, '/../lambda/hello.ts'),
+      bundling: {
+        minify: true,
+        externalModules: ['aws-sdk'],
+      },
+    });
   }
 }
