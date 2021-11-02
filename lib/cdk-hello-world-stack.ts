2c4dca38365716daaede605e306a415254e07a96
davidivad96 <davidivad96@gmail.com>
2021-11-02 21:55:26 +0100

    Clean up sample project

--- lib/cdk-hello-world-stack.ts
+++ lib/cdk-hello-world-stack.ts
@@ -1,18 +1,7 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkHelloWorldStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkHelloWorldQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkHelloWorldTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
