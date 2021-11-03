a12196d76cc0dc9b1b77ea3cbeb86e38974440af
davidivad96 <davidivad96@gmail.com>
2021-11-03 17:48:04 +0100

    Add TableViewer construct to the main stack

--- lib/cdk-hello-world-stack.ts
+++ lib/cdk-hello-world-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 import * as path from 'path';
 import { HitCounter } from './hitcounter';
 
@@ -28,6 +29,13 @@ export class CdkHelloWorldStack extends cdk.Stack {
     // defines an API Gateway REST API resource backed by our "hello" function
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler,
+    });
+
+    // defines a custom construct named Table Viewer (https://www.npmjs.com/package/cdk-dynamo-table-viewer)
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits',
     });
   }
 }
