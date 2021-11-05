102133f6467a597a17c5f52c54e1612c0b9cde32
davidivad96 <davidivad96@gmail.com>
2021-11-05 13:51:07 +0100

    Add outputs to stack so that we can see the endpoints

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -7,6 +7,9 @@ import * as path from 'path';
 import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
+  public readonly hcViewerUrl: cdk.CfnOutput;
+  public readonly hcEndpoint: cdk.CfnOutput;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -27,15 +30,24 @@ export class CdkWorkshopStack extends cdk.Stack {
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function
-    new apigw.LambdaRestApi(this, 'Endpoint', {
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler,
     });
 
     // defines a custom construct named Table Viewer (https://www.npmjs.com/package/cdk-dynamo-table-viewer)
-    new TableViewer(this, 'ViewHitCounter', {
+    const tv = new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
       table: helloWithCounter.table,
       sortBy: '-hits',
+    });
+
+    // add outputs to see the endpoints
+    this.hcEndpoint = new cdk.CfnOutput(this, 'GatewayUrl', {
+      value: gateway.url,
+    });
+
+    this.hcViewerUrl = new cdk.CfnOutput(this, 'TableViewUrl', {
+      value: tv.endpoint,
     });
   }
 }
